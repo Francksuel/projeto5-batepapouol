1b9ece3dcdae7070ef7f28434c8fe261235bac2e
Francksuel <francksuelbarbosa@gmail.com>
2022-06-26 16:06:20 -0300

    Requisitos finalizados

--- script.js
+++ script.js
@@ -6,10 +6,28 @@ let mensagens=[];
 login();
 function login() {    
     const requisicao = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", usuario);
-    requisicao.then(obterMensagens);
+    requisicao.then(refreshMensagens);
+    requisicao.catch(usuarioInvalido);
 }
+function usuarioInvalido(resposta){
+    while (resposta.response.status==400){
+        nome=prompt("Nome de usuário já existe. Tente outro:")
+usuario = {
+    name: nome,
+};
+resposta = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", usuario);
+resposta.then(refreshMensagens);
+resposta.catch(usuarioInvalido);
+}
+}
+
 function refreshMensagens(){
+    obterMensagens();
     setInterval(obterMensagens,3000);
+    setInterval(manterLogin,5000);
+}
+function manterLogin(){
+axios.post("https://mock-api.driven.com.br/api/v6/uol/status",usuario);
 }
 function obterMensagens() {  
     const promess = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
@@ -17,25 +35,41 @@ function obterMensagens() {
     promess.catch(tratarErro);
     } 
 function processarResposta(resposta) {   
-    mensagens=resposta.data;
-    console.log(mensagens);
+    mensagens=resposta.data;   
    let ulmensagens=document.querySelector(".mensagens");
    ulmensagens.innerHTML=""
    for (let i=0;i<mensagens.length;i++){ 
    if (mensagens[i].type==="status"){   
     ulmensagens.innerHTML+=`<div class="status">
-   (${mensagens[i].time})&nbsp <span>${mensagens[i].from}&nbsp</span> ${mensagens[i].text}\n</div>`       
+   <p><span class="tempo">(${mensagens[i].time})</span> <span class="bold">${mensagens[i].from}</span> ${mensagens[i].text}</p></div>`       
    }else if (mensagens[i].type==="message"){
     ulmensagens.innerHTML+=`<div class="message">
-    (${mensagens[i].time})&nbsp <span>${mensagens[i].from}</span>&nbsp para &nbsp<span>${mensagens[i].to}</span>:&nbsp${mensagens[i].text}\n</div>`
-   }else if (mensagens[i].type==="private-message"){
-    ulmensagens.innerHTML+=`<div class="private-message">
-    (${mensagens[i].time})&nbsp <span>${mensagens[i].from}</span>&nbsp para &nbsp<span>${mensagens[i].to}</span>:&nbsp${mensagens[i].text}\n</div>`
-   }   
+    <p><span class="tempo">(${mensagens[i].time})</span> <span class="bold">${mensagens[i].from}</span> para <span class="bold">${mensagens[i].to}</span>: ${mensagens[i].text}</p></div>`
+   }else if (mensagens[i].type==="private_message"){
+    if (mensagens[i].to===usuario.name){
+    ulmensagens.innerHTML+=`<div class="private_message">
+    <p><span class="tempo">(${mensagens[i].time})</span> <span class="bold">${mensagens[i].from}</span> para <span class="bold">${mensagens[i].to}</span>: ${mensagens[i].text}</p></div>`
+   }     
 }
+   }
+document.querySelector(".mensagens").lastChild.scrollIntoView();
 }
-function tratarErro(erro) {
-    if (erro.status >= 400) {
-        console.log("erro do usuário");
-    }
+function enviarMensagem(){   
+let envio=document.querySelector("input").value
+let mensenviada={
+    from:nome,
+    to:"Todos",
+    text:envio,
+    type:"message"  
+}
+document.querySelector("input").value=""
+let promess=axios.post("https://mock-api.driven.com.br/api/v6/uol/messages",mensenviada)
+promess.catch(deslogado);
+}
+function deslogado(){
+    alert("Usuário está deslogado")
+    window.location.reload()
+}
+function tratarErro(){
+    alert ("Deu erro ai")
 }
